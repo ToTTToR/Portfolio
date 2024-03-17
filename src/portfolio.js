79743caa7bf5cb8457735f88ce05070628caa367
ToTTToR <vikinor007@gmail.com>
2024-03-17 20:23:44 +0100

    Update portfolio with personal information and project details

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,7 +7,7 @@ const header = {
 const about = {
   // all the properties are optional - can be left empty or deleted
   name: 'Viktor Adrian Mouton',
-  role: 'Front End Engineer',
+  role: 'Cool engineer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
