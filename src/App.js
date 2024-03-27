ed153667fe5f555a101bd4eb2ebc32c37c77cb39
ToTTToR <vikinor007@gmail.com>
2024-03-27 15:04:12 +0100

    Trying to add routes to the app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import { ThemeContext } from './contexts/theme'
 import Header from './components/Header/Header'
 import About from './components/About/About'
@@ -8,7 +9,21 @@ import ScrollToTop from './components/ScrollToTop/ScrollToTop'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 import ProfilePic from './components/ProfilePic/ProfilePic'
+import BingusOrFloppa from './components/BingusOrFloppa/BingusOrFloppa'
 import './App.css'
+
+const MainPage = () => {
+  const a = 1;
+  return (
+    <div>
+      <ProfilePic />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </div>
+  )
+};
 
 const App = () => {
   const [{ themeName }] = useContext(ThemeContext)
@@ -24,6 +39,10 @@ const App = () => {
         <Skills />
         <Contact />
       </main>
+
+      <Routes>
+        <Route path='/bingus-or-floppa' element={<BingusOrFloppa />} />
+      </Routes>
 
       <ScrollToTop />
       <Footer />
---
+++ src/components/BingusOrFloppa/BingusOrFloppa.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BingusOrFloppa = () => {
+
+  const random = Math.random();
+  return (
+    <div>
+      <h1>This is a new page</h1>
+      <p>Welcome to the new page!</p>
+    </div>
+  );
+}
+
+export default BingusOrFloppa;
